462430ef943ec68040f9169d862a12f3d2d06b86
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2023-12-22 23:54:15 +0200

    add icons

---
+++ src/components/Icons/Plus/Plus.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Svg } from './Plus.styled';
+
+export function Plus() {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <path
+        d="M8 4V12M12 8H4"
+        stroke="#407BFF"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+}
