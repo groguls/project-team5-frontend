c8ddb828befdcaeace5c2d19ef02cccd55b22aaa
Yevhen Sydorov <eugene.sidorov@me.com>
2023-12-22 17:55:05 +0100

    add button

---
+++ src/components/Button/Button.styled.jsx
@@ -0,0 +1,36 @@
+import styled, { css } from 'styled-components';
+
+export const StyledButton = styled.button`
+  display: inline-flex;
+  padding: 10px 30px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px; /* відстань між іконкою та текстом */
+
+  border-radius: 10px;
+  border: none;
+  height: 44px;
+
+  /* Зміна бекграунду кнопки */
+  background-color: ${props => props.backgroundColor || '#407BFF'};
+
+  box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+
+  color: ${props => props.textColor || 'white'};
+  text-align: center;
+  font-family: Roboto;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 1.33;
+
+  /* Зміна ширини кнопки */
+  ${props =>
+    props.width &&
+    css`
+      width: ${props.width};
+    `}
+
+  &:hover {
+    box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
+  }
+`;
