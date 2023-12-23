bba1ab15f9dd7b27d02f221af67c269ef48984a3
EvhYa <nicetea.bro@gmail.com>
2023-12-23 15:39:28 +0200

    add components text button redirect and text button

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,14 +1,14 @@
-// import { useSelector } from 'react-redux';
-// import {
-//   selectIsLoggedIn,
-//   selectIsRefreshing,
-//   selectUser,
-// } from 'redux/auth/slice';
+import { useSelector } from 'react-redux';
+import {
+  selectIsLoggedIn,
+  selectIsRefreshing,
+  selectUser,
+} from 'redux/auth/slice';
 
 export const useAuth = () => {
-  // const user = useSelector(selectUser);
-  // const isLoggedIn = useSelector(selectIsLoggedIn);
-  // const isRefreshing = useSelector(selectIsRefreshing);
+  const user = useSelector(selectUser);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
 
-  // return { user, isLoggedIn, isRefreshing };
+  return { user, isLoggedIn, isRefreshing };
 };
