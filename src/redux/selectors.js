cfe6c59fdae494d0384d553436f7f94dcf750c4a
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2023-12-25 19:32:41 +0200

    add home page
    add home page, img and style

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectIsLoading = state => state.loading;
 
 // user selecctors
@@ -7,5 +9,10 @@ export const selectToken = state => state.auth.token;
 export const selectName = state => state.auth.user.name;
 export const selectUserLogo = state => state.auth.user.avatar;
 export const selectDailyNorma = state => state.auth.user.dailyNorma;
+
+export const selectIsAuth = createSelector(
+  [selectIsLoggedIn, selectToken],
+  (isLoggedIn, token) => token && isLoggedIn
+);
 
 export const selectIsRefreshing = state => state.auth.isRefreshing;
