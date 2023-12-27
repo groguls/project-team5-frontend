6441062ae0c8c7e90fc51956126777bf82efc321
Yevhen Sydorov <eugene.sidorov@me.com>
2023-12-27 09:12:57 +0100

    add forgotPaswwordPage component

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -6,19 +6,18 @@ export const StyledButton = styled.button`
   justify-content: center;
   align-items: center;
   gap: 8px; /* відстань між іконкою та текстом */
-
+  max-width: 280px;
   border-radius: 10px;
   border: none;
   height: 44px;
 
   /* Зміна бекграунду кнопки */
-  background-color: ${props => props.backgroundColor || '#407BFF'};
+  background-color: ${props => props.$backgroundColor || '#407BFF'};
 
   box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
 
-  color: ${props => props.textColor || 'white'};
+  color: ${props => props.$textColor || 'white'};
   text-align: center;
-  font-family: Roboto;
   font-size: 18px;
   font-weight: 500;
   line-height: 1.33;
@@ -32,5 +31,13 @@ export const StyledButton = styled.button`
 
   &:hover {
     box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
+  }
+
+  @media screen and (min-width: 768px) {
+    max-width: 336px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    max-width: 384px;
   }
 `;
