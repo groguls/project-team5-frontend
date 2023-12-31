f7a008d34635da0b115f50d02b7dca614510bb04
EvhYa <nicetea.bro@gmail.com>
2023-12-31 18:14:51 +0200

    loader with backdrop and responsive text

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -2,11 +2,13 @@ import { WaterConsumptionTracker } from 'components/WaterConsumptionTracker/Wate
 import { WhyDrinkWater } from 'components/WhyDrinkWater/WhyDrinkWater';
 import { WelcomePageContainer } from './WelcomePage.styled';
 import { MainContainer } from '../HomePage/HomePage.styled';
+import { Loader } from 'components/Loader/Loader';
 
 export default function WelcomePage() {
   return (
     <MainContainer $page="welcome">
       <WelcomePageContainer>
+        <Loader>Loading...</Loader>
         <WaterConsumptionTracker />
         <WhyDrinkWater />
       </WelcomePageContainer>
