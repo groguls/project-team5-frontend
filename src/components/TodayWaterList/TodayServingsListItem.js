02462581d3330f22ca11b2504180b26b53d91c17
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2024-01-06 22:48:34 +0200

    fixed delete water

--- src/components/TodayWaterList/TodayServingsListItem.js
+++ src/components/TodayWaterList/TodayServingsListItem.js
@@ -1,6 +1,13 @@
 import { PencilSquare } from '../Icons/PencilSquare';
+import { Delete } from '../Icons/Delete';
+import { useDispatch } from 'react-redux';
+import { deleteWater } from '../../redux/water/waterOperations';
+import { useModal } from '../ModalContextProvider/ModalContextProvider';
+import { DeleteWater } from '../TodayListModal/DeleteWater';
+import toast from 'react-hot-toast';
 
 import {
+  DeleteWaterContainer,
   StyledGlassOfWater,
   TimeServing,
   TodayData,
@@ -8,9 +15,22 @@ import {
   TodayItem,
   WaterVolume,
 } from './TodayServingsListItem.styled';
-import { Delete } from '../Icons/Delete';
 
 export const TodayServingListItem = ({ todayList }) => {
+  const dispatch = useDispatch();
+  const toggleModal = useModal();
+
+  const handleDelete = id => {
+    dispatch(deleteWater(id))
+      .unwrap()
+      .then(data => {
+        toast.success('Delete success');
+        toggleModal();
+      })
+      .catch(() => {
+        toast.error('Something went wrong');
+      });
+  };
   return (
     <>
       <TodayItem>
@@ -21,7 +41,19 @@ export const TodayServingListItem = ({ todayList }) => {
         </TodayData>
         <TodayIcons>
           <PencilSquare />
-          <Delete />
+          <DeleteWaterContainer
+            onClick={() =>
+              toggleModal(
+                <DeleteWater
+                  size={'small'}
+                  title={'Delete entry'}
+                  deleteEntry={() => handleDelete(todayList._id)}
+                />
+              )
+            }
+          >
+            <Delete />
+          </DeleteWaterContainer>
         </TodayIcons>
       </TodayItem>
     </>
