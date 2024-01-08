066044f647afbcc79d291591e9c5db6cdba6ecd8
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2024-01-08 19:14:37 +0100

    add style ModalDay

--- src/components/MonthStatsTable/DaysGeneralStats.js
+++ src/components/MonthStatsTable/DaysGeneralStats.js
@@ -1,11 +1,46 @@
+import React from 'react';
+import { ContentItemDetails, ModalContentItem, ModalContentList, ModalDate } from './DaysGeneralStats.styled';
+
 export const DaysGeneralStats = ({ selectedDay }) => {
   const { date, percentage, entries, dailyWaterRate } = selectedDay;
+
+  const formattedDateString = formatDateString(date);
+
   return (
-    <div>
-      <p>{date}</p>
-      <p>Daily norma: {dailyWaterRate}</p>
-      <p>Fulfillment of the daily norm: {percentage}</p>
-      <p>How many servings of water: {entries}</p>
-    </div>
+    <ModalContentList>
+      <ModalContentItem>
+        <ModalDate>{formattedDateString}</ModalDate>
+      </ModalContentItem>
+      <ModalContentItem>
+        Daily norma:
+        <ContentItemDetails>{dailyWaterRate}</ContentItemDetails>
+      </ModalContentItem>
+      <ModalContentItem>
+        Fulfillment of the daily norm:
+        <ContentItemDetails>{percentage}</ContentItemDetails>
+      </ModalContentItem>
+      <ModalContentItem>
+        How many servings of water:
+        <ContentItemDetails>{entries}</ContentItemDetails>
+      </ModalContentItem>
+    </ModalContentList>
   );
 };
+
+const formatDateString = (date) => {
+  const formattedDate = new Date(date);
+  const day = formattedDate.getDate();
+  const month = getMonthName(formattedDate.getMonth());
+  return `${day}, ${month}`;
+};
+
+const getMonthName = (monthNumber) => {
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+  return months[monthNumber];
+};
+
+
+
