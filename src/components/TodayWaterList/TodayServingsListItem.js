957a953590795a2de13884a39cf7d38804fdd8f7
Marini23 <marina.smile1m@gmail.com>
2024-01-08 19:57:08 +0200

    fix_today_month_styles

--- src/components/TodayWaterList/TodayServingsListItem.js
+++ src/components/TodayWaterList/TodayServingsListItem.js
@@ -25,6 +25,10 @@ export const TodayServingListItem = ({ todayList }) => {
   const dispatch = useDispatch();
   const toggleModal = useModal();
 
+  // const handleLiClick = () => {
+  //   toggleModal();
+  // };
+
   const handleDelete = id => {
     dispatch(deleteWater(id))
       .unwrap()
