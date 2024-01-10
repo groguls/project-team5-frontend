fd4c9191045be7b7e31394bb54db5dd983741f5a
Mykola-Ts <tsybulskiyk@gmail.com>
2024-01-10 19:06:25 +0200

    fixed

--- src/components/DailyNormaModal/DailyNormaForm.jsx
+++ src/components/DailyNormaModal/DailyNormaForm.jsx
@@ -1,18 +1,11 @@
 import { useDispatch } from 'react-redux';
 import { useModal } from '../ModalContextProvider/ModalContextProvider';
-import {
-  BtnSave,
-  CalculateInput,
-  FormStyles,
-  ValidationError,
-} from './DailyNormaModal.styled';
+import { BtnSave, CalculateInput, FormStyles } from './DailyNormaModal.styled';
 import { updateWaterRate } from '../../redux/auth/operations';
 import { updateDailyNorma } from '../../redux/water/waterSlice';
 import toast from 'react-hot-toast';
-import { useState } from 'react';
 
 export const DailyNormaForm = () => {
-  const [error, setError] = useState(null);
   const dispatch = useDispatch();
   const toggleModal = useModal();
 
@@ -42,26 +35,9 @@ export const DailyNormaForm = () => {
           max={15}
           step={0.1}
           required
-          onInvalid={e => {
-            e.preventDefault();
-            !e.target.value
-              ? setError('Fill in the field')
-              : setError('The value must be greater than or equal to 1');
-          }}
-          onInput={() => setError(null)}
-          onBlur={e => {
-            !e.target.value
-              ? setError('Fill in the field')
-              : e.target.value < 1
-              ? setError('The value must be greater than or equal to 1')
-              : setError(null);
-          }}
         />
-        {error && <ValidationError>{error}</ValidationError>}
       </label>
-      <BtnSave type="submit" onClick={() => setError(false)}>
-        Save
-      </BtnSave>
+      <BtnSave type="submit">Save</BtnSave>
     </FormStyles>
   );
 };
